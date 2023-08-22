4ca5983e7fff0c53a260a15ab61e9f55072b7509
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-08-22 10:53:37 -0400

    MODELS/USER: changed "username" on line 14 to "password"

--- src/models/user.js
+++ src/models/user.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
 		required: [true, "Please provide an email"],
 		unique: true,
 	},
-	username: {
+	password: {
 		type: String,
 		required: [true, "Please provide a password"],
 	},

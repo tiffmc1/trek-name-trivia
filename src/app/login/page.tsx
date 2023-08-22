7afc25d0be5a563c64910f201db0342b3b9a0a75
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-08-21 22:22:01 -0400

    LOGIN: added route for user login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,21 +1,47 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import { toast } from "react-hot-toast";
 
 export default function LoginPage() {
 	const [user, setUser] = useState({
 		email: "",
 		password: "",
 	});
+	const [buttonDisabled, setButtonDisabled] = useState(true);
+	const [loading, setLoading] = useState(false);
+	const router = useRouter();
 
-	const onLogin = async () => {};
+	useEffect(() => {
+		user.email.length > 0 && user.password.length > 0
+			? setButtonDisabled(false)
+			: setButtonDisabled(true);
+	}, [user]);
+
+	const onLogin = async () => {
+		try {
+			setLoading(true);
+
+			const response = await axios.post("/api/users/login", user);
+
+			console.log("Login Success", response.data);
+			toast.success("Login Success");
+			router.push("/profile");
+		} catch (error: any) {
+			toast.error(error.message);
+			console.log("Login Page Error:", error.message);
+		} finally {
+			setLoading(false);
+		}
+	};
 
 	return (
 		<div className="flex flex-col items-center">
-			<h1>Login</h1>
+			<h1>{loading ? "Processing" : "Login"}</h1>
 			<hr />
+
 			<label htmlFor="email">email</label>
 			<input
 				type="text"
@@ -40,7 +66,7 @@ export default function LoginPage() {
 				onClick={onLogin}
 				className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
 			>
-				Login
+				{buttonDisabled ? "No Login" : "Login"}
 			</button>
 			<Link href="/signup">Sign Up</Link>
 		</div>

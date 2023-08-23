c7ab83209ea635922f1b78292a08039f77abde48
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2023-08-23 00:27:07 -0400

    PROFILEPAGE: testing bringing in user data to display

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,20 @@
 "use client";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
+import Link from "next/link";
+
+// interface UserData {
+// 	id: string;
+// 	email: string;
+// 	username: string;
+// }
 
 export default function ProfilePage() {
 	const router = useRouter();
-
+	// const [user, setUser] = useState<any | null>(null);
+	const [user, setUser] = useState("nothing");
 	const onLogout = async () => {
 		try {
 			const logout = await axios.get("/api/users/logout");
@@ -18,10 +27,29 @@ export default function ProfilePage() {
 		}
 	};
 
+	useEffect(() => {
+		const getUserData = async () => {
+			const res = await axios.get("/api/users/user");
+			console.log(res.data);
+			setUser(res.data.data._id);
+		};
+
+		getUserData();
+	}, []);
+
+	console.log(user);
+
 	return (
 		<div>
 			<h1>Profile Page</h1>
 			<hr />
+			<h2>
+				{user ? (
+					<div>
+						<div>{user}</div>
+					</div>
+				) : null}
+			</h2>
 			<div>This is the profile page</div>
 			<hr />
 			<button
